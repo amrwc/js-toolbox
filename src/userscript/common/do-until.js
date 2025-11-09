97d3192b39113563058253d04c48002ece00c5cb
amrwc <42574496+amrwc@users.noreply.github.com>
2025-11-09 01:56:49 +0100

    Simplify JsToolbox and doUntil declarations

--- src/userscript/common/do-until.js
+++ src/userscript/common/do-until.js
@@ -1,21 +1,19 @@
-;(function (global) {
-  function doUntil({ intervalMs, maxRetries, callback }) {
-    return new Promise((resolve, reject) => {
-      let i = 0
-      const intervalId = setInterval(() => {
-        callback(res => {
-          clearInterval(intervalId)
-          resolve(res)
-        }, reject)
-        if (++i >= maxRetries) {
-          clearInterval(intervalId)
-          reject(new Error(`Reached maximum retry count: ${maxRetries}`))
-        }
-      }, intervalMs)
-    })
-  }
-  if (!global.JsToolbox) {
-    global.JsToolbox = {}
-  }
-  global.JsToolbox.doUntil = doUntil
-})(typeof unsafeWindow !== "undefined" ? unsafeWindow : window)
+if (!JsToolbox) {
+  JsToolbox = {}
+}
+
+JsToolbox.doUntil = ({ intervalMs, maxRetries, callback }) => {
+  return new Promise((resolve, reject) => {
+    let i = 0
+    const intervalId = setInterval(() => {
+      callback(res => {
+        clearInterval(intervalId)
+        resolve(res)
+      }, reject)
+      if (++i >= maxRetries) {
+        clearInterval(intervalId)
+        reject(new Error(`Reached maximum retry count: ${maxRetries}`))
+      }
+    }, intervalMs)
+  })
+}
