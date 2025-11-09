aa03b1f0123f029981f3bd5bb885c7640b45c9bc
amrwc <42574496+amrwc@users.noreply.github.com>
2025-11-09 01:27:38 +0100

    Define `do-until.js`

---
+++ src/userscript/common/do-until.js
@@ -0,0 +1,18 @@
+;(function (global) {
+  function doUntil({ intervalMs, maxRetries, callback }) {
+    return new Promise((resolve, reject) => {
+      let i = 0
+      const intervalId = setInterval(() => {
+        callback(res => {
+          clearInterval(intervalId)
+          resolve(res)
+        }, reject)
+        if (++i >= maxRetries) {
+          clearInterval(intervalId)
+          reject(new Error(`Reached maximum retry count: ${maxRetries}`))
+        }
+      }, intervalMs)
+    })
+  }
+  global.doUntil = doUntil
+})(typeof unsafeWindow !== "undefined" ? unsafeWindow : window)
